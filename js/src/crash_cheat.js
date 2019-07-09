faa595a2a6302e7e13549db5acc9664c46b40c62
ArchitecTang <hszt558842@outlook.com>
2019-07-09 21:05:02 +0800

    Site updated: 2019-07-09 21:04:51

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,17 +1,3 @@
-<!--崩溃欺骗-->
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/img/TEP.ico");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+// build time:Tue Jul 09 2019 21:03:27 GMT+0800 (中国标准时间)
+var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
+//rebuild by neat 
