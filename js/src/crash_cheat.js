75420ab2f768aca6edf25403a0d2fbe3b40eba5a
ArchitecTang <hszt558842@outlook.com>
2019-07-14 22:34:57 +0800

    Site updated: 2019-07-14 22:34:38

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sun Jul 14 2019 21:40:34 GMT+0800 (中国标准时间)
+// build time:Sun Jul 14 2019 22:34:30 GMT+0800 (中国标准时间)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
