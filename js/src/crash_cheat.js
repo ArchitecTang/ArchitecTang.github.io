00fb21a09e937aca944a4342cc1d61418166316f
architectang <hszt558842@outlook.com>
2019-07-21 14:08:54 +0800

    Site updated: 2019-07-21 14:08:33

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,3 +1,3 @@
-// build time:Sun Jul 21 2019 13:55:00 GMT+0800 (中国标准时间)
+// build time:Sun Jul 21 2019 14:08:28 GMT+0800 (中国标准时间)
 var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/favicon.ico");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
